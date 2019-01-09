7b6be8232a3cfcde1f8bfa9af50db9aaf0dba0a3
J <jevongordon@pursuit.org>
2019-01-09 12:04:34 -0500

    Initial commit

---
+++ TradingCardGame/Model/Pokemon.swift
@@ -0,0 +1,9 @@
+//
+//  Pokemon.swift
+//  TradingCardGame
+//
+//  Created by J on 1/9/19.
+//  Copyright © 2019 J. All rights reserved.
+//
+
+import Foundation
---
+++ TradingCardGame/Network Services/PokemonAPIClient.swift
@@ -0,0 +1,9 @@
+//
+//  PokemonAPIClient.swift
+//  TradingCardGame
+//
+//  Created by J on 1/9/19.
+//  Copyright © 2019 J. All rights reserved.
+//
+
+import Foundation
